f9692c6cae0a78b9788fdfd095b065f7db7a72e6
MeGoogle <manonhorizon@gmail.com>
2023-08-26 18:23:30 +0300

    Basic export test

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fly-guy-p5-js
+This repository contains the logic for p5-js "Fly guy" game
---
+++ main.js
@@ -0,0 +1,9 @@
+import { basicValue } from './variables.js';
+
+const consoleFunction = () => {
+  console.log('basicValue:', basicValue);
+}
+
+module.exports = {
+  consoleFunction,
+};
